3f5f012eb1392a2a61c74d7d981066b4915d459f
Nawafbnahmed1 <nwafbnahmd12@gmail.com>
2026-07-24 23:52:11 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'theme/app_theme.dart';
-import 'screens/navigation_screen.dart'; // إرجاع الشاشة الرئيسية للأقسام
+import 'screens/navigation_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  // الاحتفاظ بتهيئة قاعدة البيانات الصحيحة لكي يربطها المطور غداً بسهولة
   await Supabase.initialize(
-    url: 'https://supabase.co',
+    url: 'https://njdeduovwamcbcucmull.supabase.co',
     publishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZGVkdW92d2FtY2JjdWNtdWxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxODM3NDgsImV4cCI6MjA5OTc1OTc0OH0.AeQM-jGZOQrufBMmav7SxPK93LUi2DqEeSi2O95esq0',
   );
   
@@ -23,7 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'NF Sports',
       theme: AppTheme.darkTheme,
-      home: NavigationScreen(), // العودة لفتح كافة الأقسام والصفحات السابقة تلقائياً
+      home: NavigationScreen(),
     );
   }
 }
