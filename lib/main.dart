ee53616eacfe42830e82b067ff1d9a4904e59412
Nawafbnahmed1 <nwafbnahmd12@gmail.com>
2026-07-25 17:07:37 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ Future<void> main() async {
   
   // الاحتفاظ بالروابط والمفاتيح الأصلية والكاملة 100% بدون أي نقص
   await Supabase.initialize(
-    url: 'https://supabase.co',
+    url: url: 'https://njdeduovwamcbcucmull.supabase.co',
     publishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZGVkdW92d2FtY2JjdWNtdWxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxODM3NDgsImV4cCI6MjA5OTc1OTc0OH0.AeQM-jGZOQrufBMmav7SxPK93LUi2DqEeSi2O95esq0',
   );
   
