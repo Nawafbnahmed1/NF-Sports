45b8746af823ed40e913e7c5dfb0f8343b5904cc
Nawafbnahmed1 <nwafbnahmd12@gmail.com>
2026-07-27 22:00:09 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,13 +1,15 @@
+// 1. الطريقة الصحيحة لتعريف المتغيرات في ملفات الـ .kts الحديثة
+val kotlinVersion by extra("1.9.0")
+
 buildscript {
-    ext.kotlin_version = "1.9.0"
     repositories {
         google()
         mavenCentral()
     }
-
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("com.android.tools.build:gradle:8.1.0") // تأكد من مطابقة إصدار الجريدل لديك
+        // 2. استدعاء المتغير بالصيغة البرمجية الصحيحة لـ Kotlin
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
 
@@ -16,8 +18,4 @@ allprojects {
         google()
         mavenCentral()
     }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
 }
