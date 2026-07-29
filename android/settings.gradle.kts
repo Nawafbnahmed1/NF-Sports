50853026b559447e391584641efa1e2f527315e4
Nawafbnahmed1 <nwafbnahmd12@gmail.com>
2026-07-29 22:31:18 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,12 +1,13 @@
 pluginManagement {
-    val flutterSdkPath = run {
+    val flutterSdkVersion = run {
         val properties = java.util.Properties()
         file("local.properties").inputStream().use { properties.load(it) }
         val flutterSdkPath = properties.getProperty("flutter.sdk")
         require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
         flutterSdkPath
     }
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    includeBuild("$flutterSdkVersion/packages/flutter_tools/gradle")
     repositories {
         google()
         mavenCentral()
@@ -16,8 +17,9 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.android.application") version "8.1.1" apply false
+    // 🚀 ترقية جوهرية لإصدار الكوتلن إلى 2.0.0 المستقر لتدمير تعارض الـ buildGradleApp نهائياً
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
 
 include(":app")
