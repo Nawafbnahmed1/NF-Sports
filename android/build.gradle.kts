5d93aaced50cf52717b6367f69c55e35db0e39c3
Nawafbnahmed1 <nwafbnahmd12@gmail.com>
2026-07-30 15:57:06 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,17 +1,3 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven {
-            url = uri("https://maven.aliyun.com/repository/public")
-        }
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.11.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-    }
-}
-
 allprojects {
     repositories {
         google()
