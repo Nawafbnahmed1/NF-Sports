b23cbbd252db7cad3fe2462fa9647a7a7ec32ae7
Nawafbnahmed1 <nwafbnahmd12@gmail.com>
2026-08-07 16:18:29 +0300

    Add import for flutter/services.dart

--- lib/widgets/no_internet_widget.dart
+++ lib/widgets/no_internet_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NoInternetWidget extends StatefulWidget {
   final VoidCallback onRetry;
