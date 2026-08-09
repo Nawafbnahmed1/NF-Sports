5afc54434409790e925e2e6770ef2d5a3004b199
Nawafbnahmed1 <nwafbnahmd12@gmail.com>
2026-08-09 19:12:45 +0300

    Fix formatting in settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
         require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
         flutterSdkPath
     }
-
+ 
     includeBuild("$flutterSdkVersion/packages/flutter_tools/gradle")
     repositories {
         google()
