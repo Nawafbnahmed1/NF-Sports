0f0a670626926e4458ef13429c26729a497c5365
Nawafbnahmed1 <nwafbnahmd12@gmail.com>
2026-08-09 22:01:20 +0300

    Remove unnecessary blank line in app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
 import 'screens/home_screen.dart';
-
+ 
 class NFApp extends StatelessWidget {
   const NFApp({
     super.key,
