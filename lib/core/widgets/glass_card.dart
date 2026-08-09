f9c00c18fa9d3369f3fc201a772b7bb8122e874d
Nawafbnahmed1 <nwafbnahmd12@gmail.com>
2026-08-09 20:36:24 +0300

    Remove unnecessary blank line in glass_card.dart

--- lib/core/widgets/glass_card.dart
+++ lib/core/widgets/glass_card.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import '../constants/app_colors.dart';
-
+ 
 class GlassCard extends StatelessWidget {
   final Widget child;
   final EdgeInsetsGeometry? padding;
