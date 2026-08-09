5d0304f8867092c853e4349332e7dc7ba21f5e3e
Nawafbnahmed1 <nwafbnahmd12@gmail.com>
2026-08-09 21:27:58 +0300

    Clean up whitespace in lineup_service.dart
    Removed unnecessary whitespace in lineup_service.dart.

--- lib/services/lineup_service.dart
+++ lib/services/lineup_service.dart
@@ -3,7 +3,7 @@ import '../models/lineup_model.dart';
 
 class LineupService {
   final SupabaseClient _client = Supabase.instance.client;
-
+  
   /// جلب تشكيلات وخطط مباراة محددة بواسطة المعرف حياً من السيرفر
   Future<List<LineupModel>> getMatchLineups(String matchId) async {
     try {
@@ -19,7 +19,7 @@ class LineupService {
       throw Exception('Failed to load lineups: $e');
     }
   }
- 
+
   /// إضافة تشكيلة جديدة (خاص بلوحة تحكم الإدارة حقتك سحابياً)
   Future<void> addLineup(LineupModel lineup) async {
     try {
