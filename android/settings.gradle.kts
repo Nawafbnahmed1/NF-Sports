cda94c1b7d62e767900de13eb82ee912e22a7f21
Nawafbnahmed1 <nwafbnahmd12@gmail.com>
2026-08-12 22:33:48 +0300

    Update Android Gradle Plugin to version 8.11.1

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -6,20 +6,18 @@ pluginManagement {
         require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
         flutterSdkPath
     }
- 
-    includeBuild("$flutterSdkVersion/packages/flutter_tools/gradle")
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
+
+    includeBuild("$flutterSdkVersion/packages/flutter_tools/gradle")  
+    repositories {  
+        google()  
+        mavenCentral()  
+        gradlePluginPortal()  
     }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    // 🚀 الترقية الجراحية لـ AGP إلى 8.9.1 لتلبية شروط مكتبات AndroidX الحديثة لعام 2026
-    id("com.android.application") version "8.9.1" apply false
-    // ✓ الإبقاء على إصدار لغة الكوتلن المستقر 2.2.20 بدون تغيير بناءً على خطة المساعد
+    id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
