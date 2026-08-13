6fd2aca946e197d656f8dba0463b48fa3c04d5e3
Nawafbnahmed1 <nwafbnahmd12@gmail.com>
2026-08-13 10:36:32 +0300

    Refactor settings.gradle.kts for clarity

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,23 +1,34 @@
 pluginManagement {
     val flutterSdkVersion = run {
         val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
+
+        file("local.properties").inputStream().use {
+            properties.load(it)
+        }
+
         val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+
+        require(flutterSdkPath != null) {
+            "flutter.sdk not set in local.properties"
+        }
+
         flutterSdkPath
     }
 
-    includeBuild("$flutterSdkVersion/packages/flutter_tools/gradle")  
-    repositories {  
-        google()  
-        mavenCentral()  
-        gradlePluginPortal()  
+    includeBuild("$flutterSdkVersion/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+
     id("com.android.application") version "8.11.1" apply false
+
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
